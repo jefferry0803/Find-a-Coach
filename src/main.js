447a76d30ff615fd729715e91fcbc998f9ea50f3
Jefferry Chiu <jefferrychiu.gmail.com>
2022-04-18 00:00:25 +0800

    add authentication

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 
-import router from './route.js';
+import router from './router.js';
 import store from './store/index.js';
 import App from './App.vue';
 import BaseCard from './components/ui/BaseCard.vue';
